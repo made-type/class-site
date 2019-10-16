a0f8e3f59ea22626393a5c8c19917d7d5ad25981
cody <graphichopper@gmail.com>
2019-10-15 22:50:47 -0400

    final open sans

--- projects/open-sans/js/function.js
+++ projects/open-sans/js/function.js
@@ -7,9 +7,9 @@ window.onload = function() {
   }, 100);
 }
 
-$('.front-on-click').click(function(){
-    $('.front').removeClass('front');
-    $(this).addClass('front');
+$('.front-on-click').click(function() {
+  $('.front').removeClass('front');
+  $(this).addClass('front');
 });
 
 
@@ -17,21 +17,21 @@ const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"
 const currentTheme = localStorage.getItem('theme');
 
 if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
+  document.documentElement.setAttribute('data-theme', currentTheme);
 
-    if (currentTheme === 'dark') {
-        toggleSwitch.checked = true;
-    }
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+  }
 }
 
 function switchTheme(e) {
-    if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
-    }
-    else {        document.documentElement.setAttribute('data-theme', 'light');
-          localStorage.setItem('theme', 'light');
-    }
+  if (e.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
+  }
 }
 
 toggleSwitch.addEventListener('change', switchTheme, false);
